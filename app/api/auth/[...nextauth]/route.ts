e647248949701b6239507fefddfe3748852a8aac
Alexander Aspman <aspmanalexander@gmail.com>
2024-06-20 12:36:14 +0200

    feature://added next-auth and api/routes created  auth.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+/*import { LoginOptions, handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { NextRequest } from 'next/server';
+
+const getLoginState = (req: NextRequest, loginOptions: LoginOptions) => {
+  return { basket_id: getBasketId(req) };
+};
+
+export default handleAuth({
+  login: handleLogin({ getLoginState })
+});
+
+function getBasketId(req: NextRequest) {
+    throw new Error('Function not implemented.');
+}
+
+*/
